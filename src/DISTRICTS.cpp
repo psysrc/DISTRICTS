2bbf0a18599fb445cb8b2eff63db42d17a326cc0
psysrc <sam.copping@yahoo.com>
2019-07-14 12:30:16 +0100

    Adding Hello World project as a starting point

---
+++ src/DISTRICTS.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : DISTRICTS.cpp
+// Author      : Samuel Copping
+// Version     :
+// Copyright   : 
+// Description : A procedurally generated base building game.
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World, " << endl << "Welcome to DISTRICTS." << endl; // prints Hello World
+	return 0;
+}
