edbea3db913fc3673e2c67bfa5f78800c4967a24
Sam <sam.copping@yahoo.com>
2019-08-26 22:02:56 +0100

    Added method to District so that entities can be dynamically created
    The method at this point is untested

--- src/District.h
+++ src/District.h
@@ -26,9 +26,26 @@ public:
 	bool validTileIndex(const int index) const;
 	void createBiome(int i, int j, TileProperty biomeProperty, int size);
 	void simulate();
+	template <class E> E* makeEntity();
 	std::string getName() const;
 	Tile** getTiles() const;
 	Tile* getTile(const int i, const int j) const;
 };
+
+/*
+ * Creates a new Entity in the District.
+ * Returns a pointer to the newly created Entity.
+ */
+template <class E>
+E* District::makeEntity() {
+	static_assert(std::is_base_of<Entity, E>::value, "E must extend Entity");
+
+	std::unique_ptr<E> upE = std::make_unique<E>(this);
+	E* pE = upE.get();
+
+	entities.push_back(std::move(upE));
+
+	return pE;
+}
 
 #endif /* DISTRICT_H_ */
