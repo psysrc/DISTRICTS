58b6a02aeb0ce9899b3d0bfa560e03bf082bdd2e
Sam <sam.copping@yahoo.com>
2019-09-21 20:59:11 +0100

    Increased sapling time to grow to minimum 480 and max 1440 ticks

--- src/entities/Sapling.cpp
+++ src/entities/Sapling.cpp
@@ -5,8 +5,8 @@
 #include "tasks/CutDownTree.h"
 #include "entities/Tree.h"
 
-const int maxTicksToGrow = 90;	// 6 mins
-const int minTicksToGrow = 30;	// 2 mins
+const int maxTicksToGrow = 1440;	// 6 mins
+const int minTicksToGrow = 480;		// 2 mins
 
 Sapling::Sapling(District* district) : Entity(district, "Tree Sapling", ',') {
 	ticksToGrow = minTicksToGrow + (rand() % (maxTicksToGrow - minTicksToGrow + 1));
