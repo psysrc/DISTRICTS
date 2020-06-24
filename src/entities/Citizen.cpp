e8fdd5366aad5a49f39a36d250aae3f3fec68dad
Sam <sam.copping@yahoo.com>
2020-06-24 19:48:59 +0100

    Cleaned up Citizen class

--- src/entities/Citizen.cpp
+++ src/entities/Citizen.cpp
@@ -1,11 +1,5 @@
 #include "Citizen.h"
 
-#include "tasks/Task.h"
-#include "pathfinding/Path.h"
-#include "pathfinding/PathFinding.h"
-#include "game/District.h"
-#include "ui/UI.h"
-#include "game/Tile.h"
 #include "components/PositionComponent.h"
 #include "components/WalkComponent.h"
 #include "components/DrawComponent.h"
@@ -23,70 +17,3 @@ Citizen::Citizen() : Entity("Citizen", 'C') {
 }
 
 Citizen::~Citizen() {}
-
-/*
- * Called once per game tick.
- * This prompts the citizen to do something in the world (even if it's just moving about randomly).
- */
-void Citizen::simulate() {
-//	if (upCurrentPath == nullptr)
-//		upCurrentPath = PathFinding::findPath(pTile, pDistrict->getTile(rand() % DISTRICT_SIZE, rand() % DISTRICT_SIZE));
-//	else {
-//		Tile* nextTile = upCurrentPath->next();
-//
-//		if (nextTile != nullptr)
-//			move(nextTile);
-//		else
-//			upCurrentPath = nullptr;
-//	}
-
-	// if (pCurrentTask != nullptr && upCurrentPath.get() != nullptr) {	// Citizen has a task and knows how to get to it
-	// 	if (pCurrentTask->citizenInRange(this)) {
-	// 		pCurrentTask->workOn(this);
-
-	// 		if (pCurrentTask->isCompleted())
-	// 			pCurrentTask = nullptr;
-	// 	}
-	// 	else {
-	// 		move(upCurrentPath->next());	// Move towards the task (NOTE: No nullptr checks! Shouldn't be an issue though.)
-	// 	}
-	// }
-	// else {
-	// 	pCurrentTask = pDistrict->getOldestTask();
-
-	// 	if (pCurrentTask == nullptr) {
-	// 		UI::displayActivityMessage("Citizen has nothing to do.");
-	// 	}
-	// 	else {
-	// 		upCurrentPath = PathFinding::findPath(pTile, pCurrentTask->getTile());
-
-	// 		if (upCurrentPath.get() == nullptr) {
-	// 			// Latest task cannot be reached
-	// 			UI::displayActivityMessage("Citizen can't get to the latest task.");
-	// 		}
-	// 	}
-	// }
-}
-
-/*
- * Attempts to move the Citizen to a neighbouring tile in a given direction.
- */
-void Citizen::move(int direction) {
-	// move(pTile->getNeighbourTile(direction));
-}
-
-/*
- * Attempts to move the Citizen to another tile.
- * NOTE: There are currently no checks done to ensure the Citizen moves in a legal way
- * 		-> i.e. the Citizen shouldn't be able to jump all over the map
- */
-void Citizen::move(Tile* const moveToTile) {
-	// if (moveToTile != nullptr) {				// If the other tile is valid
-	// 	if (moveToTile->citizenEnter(this)) {	// Attempt to move to the other tile
-	// 		// If successful, remember which tile we are now occupying and inform the old tile of the change
-	// 		pTile->vacateEntity();
-	// 		pTile = moveToTile;
-	// 	}
-	// }
-}
-
--- src/entities/Citizen.h
+++ src/entities/Citizen.h
@@ -2,24 +2,11 @@
 #define CITIZEN_H_
 
 #include "Entity.h"
-#include <string>
-
-class Path;
-class Tile;
-
-namespace Tasks {
-class Task;
-}
 
 class Citizen : public Entity {
-private:
-	Tasks::Task* pCurrentTask;
 public:
 	Citizen();
 	virtual ~Citizen();
-	virtual void simulate();
-	void move(int direction);
-	void move(Tile* const moveToTile);
 };
 
 #endif /* CITIZEN_H_ */
