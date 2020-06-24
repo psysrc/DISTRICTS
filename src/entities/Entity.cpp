af17f6f0d204357077e3bacc227da3a1f4019442
Sam <sam.copping@yahoo.com>
2020-06-24 21:11:06 +0100

    Implemented Entity's default constructor - should have been implemented already but this fell through the cracks until now

--- src/entities/Entity.cpp
+++ src/entities/Entity.cpp
@@ -1,13 +1,10 @@
 #include "Entity.h"
 
-Entity::Entity(std::string objectName)
-: name(objectName) {
+Entity::Entity() : name("") {}
 
-}
+Entity::Entity(std::string objectName) : name(objectName) {}
 
-Entity::~Entity() {
-
-}
+Entity::~Entity() {}
 
 std::string Entity::getName() const {
 	return name;
