7b209a28ff96fbb1b039b0d01d921ed340961b6d
Sam <sam.copping@yahoo.com>
2024-06-26 18:53:56 +0100

    Pigeons now rendered as 'p'instead of '5'

--- src/entities/Pigeon.cpp
+++ src/entities/Pigeon.cpp
@@ -12,7 +12,7 @@ std::unique_ptr<Entity> makePigeon(TileCoordinates coords)
 	pigeon->addComponent(std::make_unique<PositionComponent>(coords, pigeon.get()));
 	pigeon->addComponent(std::make_unique<OccupySpaceComponent>());
 	pigeon->addComponent(std::make_unique<RandomWanderComponent>(4, 40));
-	pigeon->addComponent(std::make_unique<RenderComponent>('5'));
+	pigeon->addComponent(std::make_unique<RenderComponent>('p'));
 
 	return pigeon;
 }
