755667df5d3f2d9c769de9747da4a92c329a0e17
Sam <sam.copping@yahoo.com>
2024-06-28 00:04:25 +0100

    Implemented the WorkSystem and WorkOnTaskComponent

---
+++ src/components/WorkOnTaskComponent.h
@@ -0,0 +1,16 @@
+#ifndef WORK_ON_TASK_COMPONENT_H_
+#define WORK_ON_TASK_COMPONENT_H_
+
+#include "components/Component.h"
+#include "game/ID.h"
+
+struct WorkOnTaskComponent : public Component
+{
+    WorkOnTaskComponent(ID_t id) : entityID(id) {}
+
+    virtual ~WorkOnTaskComponent() = default;
+
+    ID_t entityID;
+};
+
+#endif /* WORK_ON_TASK_COMPONENT_H_ */
--- src/gamesystems/WorkSystem.cpp
+++ src/gamesystems/WorkSystem.cpp
@@ -3,44 +3,66 @@
 #include <memory>
 #include "entities/Entity.h"
 #include "game/District.h"
-#include "components/WorkerComponent.h"
+#include "components/WorkOnTaskComponent.h"
 #include "components/PositionComponent.h"
+#include "components/MoveComponent.h"
+#include "components/TaskComponent.h"
 #include "pathfinding/PathFinding.h"
 
 WorkSystem::WorkSystem() {}
 
 WorkSystem::~WorkSystem() {}
 
-void WorkSystem::run(District *)
+void WorkSystem::run(District * district)
 {
-    // for (const std::unique_ptr<Entity> &upEntity : pDistrict->getEntities())
-    // {
-    //     WorkerComponent *pWC = upEntity->getComponent<WorkerComponent>();
-    //     PositionComponent *pPC = upEntity->getComponent<PositionComponent>();
+    for (const std::unique_ptr<Entity> &entity : district->getEntities())
+    {
+        WorkOnTaskComponent *cmptWorkOnTask = entity->getComponent<WorkOnTaskComponent>();
+        if (!cmptWorkOnTask)
+            continue;
 
-    //     if (pWC != nullptr && pWC->working && pPC != nullptr)
-    //     {
-    //         // If the worker's current task exists (isn't empty and hasn't been deleted by the district)
-    //         if (auto spCurrentTask = pWC->wpCurrentTask.lock())
-    //         {
-    //             // Check if the entity is within range to work on the task
-    //             if (PathFinding::euclideanDistance(pDistrict->getTile(pPC->getPosition()), spCurrentTask->getTile()) < 1.5)
-    //             {
-    //                 spCurrentTask->workOn(pDistrict);
-    //             }
+        Entity* const taskEntity = district->getEntity(cmptWorkOnTask->entityID);
+        if (!taskEntity)
+        {
+            // If this WorkOnTask component refers to a non-existent entity, delete the WorkOnTask component
+            entity->removeComponent<WorkOnTaskComponent>();
+            continue;
+        }
 
-    //             // If the task has been completed, it can be removed from the district and this worker can clear its current task
-    //             if (spCurrentTask->isCompleted())
-    //             {
-    //                 pDistrict->deleteTask(spCurrentTask.get());
-    //                 pWC->wpCurrentTask.reset();
-    //             }
-    //         }
-    //         else
-    //         {
-    //             // Clear the current task for this worker in case it has been deleted from the district
-    //             pWC->wpCurrentTask.reset();
-    //         }
-    //     }
-    // }
+        TaskComponent* const cmptTask = taskEntity->getComponent<TaskComponent>();
+        if (!cmptTask)
+        {
+            // If this WorkOnTask component refers to an entity that doesn't have a task, delete the WorkOnTask component
+            entity->removeComponent<WorkOnTaskComponent>();
+            continue;
+        }
+
+        const PositionComponent* const cmptPositionEntity = entity->getComponent<PositionComponent>();
+        const PositionComponent* const cmptPositionTask = taskEntity->getComponent<PositionComponent>();
+        if (!cmptPositionEntity || !cmptPositionTask)
+            continue;
+
+        const TileCoordinates entityPosition = cmptPositionEntity->getPosition();
+        const TileCoordinates taskPosition = cmptPositionTask->getPosition();
+
+        // If the entity is not in range to work on the task, wait until it is closer
+        if (PathFinding::euclideanDistance(entityPosition, taskPosition) > 1.5)
+        {
+            if (!entity->hasComponent<MoveComponent>())
+            {
+                entity->addComponent(std::make_unique<MoveComponent>(taskPosition, false));
+            }
+
+            continue;
+        }
+
+        --(cmptTask->workRemaining);  // Entity is in range of the task - do one unit of work
+
+        if (cmptTask->workRemaining == 0)
+        {
+            cmptTask->onCompletion();
+            taskEntity->removeComponent<TaskComponent>();
+            entity->removeComponent<WorkOnTaskComponent>();
+        }
+    }
 }
